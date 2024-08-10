7f53daa148ca3ab2edce30cb2ccb31c5f800abcf
ignacio-gavilanes <ignacio@neocoast.com>
2024-08-11 01:20:10 +0200

    Add auto-save while maintaining formating on save

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,6 @@
+return {
+	"okuuva/auto-save.nvim", -- Forked from pocco81/auto-save.nvim (has not being maintained for a while now)
+	cmd = "ASToggle",
+	event = { "InsertLeave", "TextChanged" },
+	opts = {},
+}
