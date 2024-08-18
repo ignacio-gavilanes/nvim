9a49515d571a6fe6515e49cc2bdce278cfb751ca
ignacio-gavilanes <ignacio@neocoast.com>
2024-08-18 21:17:08 +0200

    Open NeoTree on startup

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,7 +1,13 @@
-vim.api.nvim_create_autocmd("TextYankPost", { -- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", { -- Highlight when yanking text
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", { -- Open NeoTree when Neovim loads
+	desc = "Open NeoTree on startup",
+	group = vim.api.nvim_create_augroup("kickstart-neotree", { clear = true }),
+	command = ":Neotree reveal filesystem left toggle",
 })
