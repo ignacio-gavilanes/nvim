29a81442adb82f672ace9bb702604166b069e000
ignacio-gavilanes <ignacio@neocoast.com>
2024-08-19 00:59:19 +0200

    Center cursor when moving up and down

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Center cursor position when moving up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
