bcd38b6d76e826f0bae1eaf73700598e41acee67
ignacio-gavilanes <ignacio@neocoast.com>
2024-08-20 21:20:49 +0200

    Add Noice for notifications

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,5 +2,9 @@ return {
 	"okuuva/auto-save.nvim", -- Forked from pocco81/auto-save.nvim (has not being maintained for a while now)
 	cmd = "ASToggle",
 	event = { "InsertLeave", "TextChanged" },
-	opts = {},
+	opts = {
+		execution_message = {
+			enabled = false, -- disable the auto-save message
+		},
+	},
 }
