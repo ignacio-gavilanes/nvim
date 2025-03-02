d8e93bd01521f3401a19ae2911b138a2271a5f96
ignacio@neocoast.com <ignacio@neocoast.com>
2025-03-02 02:28:08 +0100

    Fix auto-save config

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,4 +2,5 @@ return {
 	"okuuva/auto-save.nvim", -- Forked from pocco81/auto-save.nvim (has not being maintained for a while now)
 	cmd = "ASToggle",
 	event = { "InsertLeave", "TextChanged" },
+	opts = {},
 }
