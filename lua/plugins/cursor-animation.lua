ac09fec85a08b99a230f88e8dc7ae274884cc61a
Nacho <ignaciogavilanes@Nachos-MacBook-Air.local>
2025-07-16 11:38:28 +0200

    [feature/cursor] - Add cursor smear animation

---
+++ lua/plugins/cursor-animation.lua
@@ -0,0 +1,9 @@
+return {
+    --[[ TODO:
+    Build Ghostty nightly (latest dev build with experimental features),
+    add cursor shader for terminal-level smear effect.
+    Afterward, this plugin is no longer necessary.
+    ]]
+	"sphamba/smear-cursor.nvim",
+    opts = {},
+}
